06fa4082a3119c006cabd7a7475a665a7c6f3db0
lilybrongo <144718640+lilybrongo@users.noreply.github.com>
2024-09-24 12:32:03 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
+//function insertionSort(arr) {
+  //for(var i = 1; i < arr.length; i++) {
+    //var val = arr[i];
+    //var j;
+    //for(j = i; j > 0 && arr[j-1] > val; j--) {
+      //arr[j] = arr[j-1];
+    //}
+    //arr[j] = val;
+  //}
+  //return arr;
+//}
+
+
 function insertionSortReverse(array) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        var val = arr[i];
+        var j;
+        for (j = i; j < arr.length - 1 && arr[j+1] < val; j++) {
+            arr[j] = arr[j + 1];
+        }
+        arr [j] = val;
+    }
     return array;
 }
